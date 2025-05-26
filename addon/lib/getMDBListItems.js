5d1c5d2009bcf2485977d310e33a39d5b876df24
LastpostNL <jhonvdborght@gmail.com>
2025-05-26 20:05:47 +0200

    Create getMDBListItems.js

---
+++ addon/lib/getMDBListItems.js
@@ -0,0 +1,34 @@
+const fetch = require("node-fetch");
+const { parseMDBListItemsToStremioItems } = require("./parseProps");
+
+async function getMDBListItems({ listid, limit = 100, offset = 0, apikey, filter_genre, sort, order, unified = true }) {
+  if (!apikey) {
+    throw new Error("API key is required");
+  }
+  if (!listid) {
+    throw new Error("listid is required");
+  }
+
+  const params = new URLSearchParams({
+    limit,
+    offset,
+    apikey,
+    unified,
+  });
+
+  if (filter_genre) params.append("filter_genre", filter_genre);
+  if (sort) params.append("sort", sort);
+  if (order) params.append("order", order);
+
+  const url = `https://api.mdblist.com/list/${listid}/items?${params.toString()}`;
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`MDBList API error: ${res.status} ${res.statusText}`);
+  }
+  const data = await res.json();
+
+  return parseMDBListItemsToStremioItems(data);
+}
+
+module.exports = getMDBListItems;
