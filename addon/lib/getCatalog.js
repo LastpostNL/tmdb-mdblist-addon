1322b36425a4b0dc64bbf14c5b3466f2096ce4d2
LastpostNL <jhonvdborght@gmail.com>
2025-05-26 18:43:25 +0200

    Update getCatalog.js

--- addon/lib/getCatalog.js
+++ addon/lib/getCatalog.js
@@ -10,9 +10,7 @@ async function getCatalog(type, language, page, id, genre, config) {
   const genreList = await getGenreList(language, type);
   const parameters = await buildParameters(type, language, page, id, genre, genreList, config);
 
-  const fetchFunction = type === "movie"
-    ? moviedb.discoverMovie.bind(moviedb)
-    : moviedb.discoverTv.bind(moviedb);
+  const fetchFunction = type === "movie" ? moviedb.discoverMovie.bind(moviedb) : moviedb.discoverTv.bind(moviedb);
 
   return fetchFunction(parameters)
     .then((res) => ({
@@ -23,7 +21,7 @@ async function getCatalog(type, language, page, id, genre, config) {
 
 async function buildParameters(type, language, page, id, genre, genreList, config) {
   const languages = await getLanguages();
-  const parameters = { language, page, 'vote_count.gte': 10 };
+  const parameters = { language, page, 'vote_count.gte': 10 };;
 
   if (config.ageRating) {
     switch (config.ageRating) {
@@ -33,26 +31,17 @@ async function buildParameters(type, language, page, id, genre, genreList, confi
         break;
       case "PG":
         parameters.certification_country = "US";
-        parameters.certification = type === "movie"
-          ? ["G", "PG"].join("|")
-          : ["TV-G", "TV-PG"].join("|");
+        parameters.certification = type === "movie" ? ["G", "PG"].join("|") : ["TV-G", "TV-PG"].join("|");
         break;
       case "PG-13":
         parameters.certification_country = "US";
-        parameters.certification = type === "movie"
-          ? ["G", "PG", "PG-13"].join("|")
-          : ["TV-G", "TV-PG", "TV-14"].join("|");
+        parameters.certification = type === "movie" ? ["G", "PG", "PG-13"].join("|") : ["TV-G", "TV-PG", "TV-14"].join("|");
         break;
       case "R":
         parameters.certification_country = "US";
-        parameters.certification = type === "movie"
-          ? ["G", "PG", "PG-13", "R"].join("|")
-          : ["TV-G", "TV-PG", "TV-14", "TV-MA"].join("|");
+        parameters.certification = type === "movie" ? ["G", "PG", "PG-13", "R"].join("|") : ["TV-G", "TV-PG", "TV-14", "TV-MA"].join("|");
         break;
       case "NC-17":
-        // no certification param set here
-        break;
-      default:
         break;
     }
   }
@@ -61,7 +50,7 @@ async function buildParameters(type, language, page, id, genre, genreList, confi
     const provider = findProvider(id.split(".")[1]);
 
     parameters.with_genres = genre ? findGenreId(genre, genreList) : undefined;
-    parameters.with_watch_providers = provider.watchProviderId;
+    parameters.with_watch_providers = provider.watchProviderId
     parameters.watch_region = provider.country;
     parameters.with_watch_monetization_types = "flatrate|free|ads";
   } else {
@@ -85,7 +74,6 @@ async function buildParameters(type, language, page, id, genre, genreList, confi
         break;
     }
   }
-
   return parameters;
 }
 
@@ -94,8 +82,8 @@ function findGenreId(genreName, genreList) {
   return genreData ? genreData.id : undefined;
 }
 
-function findLanguageCode(languageName, languages) {
-  const language = languages.find(lang => lang.name === languageName);
+function findLanguageCode(genre, languages) {
+  const language = languages.find((lang) => lang.name === genre);
   return language ? language.iso_639_1.split("-")[0] : "";
 }
 
